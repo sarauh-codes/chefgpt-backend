ac79f6e367230df3d7095669e6a32e8c2c6cac31
Sarah A <sarahannisa1411@gmail.com>
2026-03-14 17:27:36 +0800

    fix port binding for repo

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -338,6 +340,7 @@ def search_recipes():
     return jsonify({'recipes': recipes_list})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
